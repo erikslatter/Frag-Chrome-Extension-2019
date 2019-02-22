da3469686660f6279a88d0e113b2ecf33647fc4e
erikslatter <39570706+erikslatter@users.noreply.github.com>
2019-02-21 20:26:19 -0700

    update

--- extension/content.js
+++ extension/content.js
@@ -1,17 +1,15 @@
-function findRanks(i) {
-	if(window.location.href.indexOf("match") > -1) {
-      var allUsers = $(document).find("#body-match-total" + i + " tr");   
-      $.each($(document).find("#body-match-total" + i + " tr"), function(index, value){
-        var userLink = "https://play.esea.net/users/" + allUsers[index].children[0].children[1].innerHTML
-        $.get(userLink, function(data) {
-            data = data.replace(/<img[^>]*>/g,"");
-            var parsed = $('<div/>').append(data);
-            rank = $(parsed).find("#rankGraph h1").text();
-            allUsers[index].children[0].children[1].innerHTML += " (" + rank + ") ";          
+function findRanks() {
+    if (window.location.href.indexOf("match") > -1) {
+        var allUsers = $('a.sc-bdVaJa.jfbJeP');
+        $(allUsers).each(function (index, value) {
+          $.get(value, function (data) {
+              var rank = $(data).find("div.sc-bdVaJa.bBuUFV").innerHTML;
+              allUsers[index].innerHTML += " (" + rank + ")";         
         });
-    });
+        });
   }
 }
 
-findRanks(1);
-findRanks(2);
+if (window.location.href.indexOf("match") > -1) {
+    setTimeout(findRanks, 1000);
+}
