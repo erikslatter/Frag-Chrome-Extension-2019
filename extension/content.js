deca710b1d1372b45a1de484e166891838e29ea4
erikslatter <39570706+erikslatter@users.noreply.github.com>
2019-02-24 11:11:51 -0700

    more legacy fixes

--- extension/content.js
+++ extension/content.js
@@ -17,12 +17,12 @@
 
 function swapBlacksForLegacy() {    
     $('*').each(function (index, value) {
-        if ($(this).css("color") == "rgb(0, 0, 0)")
+        if ($(this).css("color") == "rgb(0, 0, 0)" || $(this).css("color") == "rgb(64, 65, 68)")
             $(this).css("color", "#f5f5f5");
     });
-
     $('*').each(function (index, value) {
-        if ($(this).css("background-color") == "rgb(255, 255, 255)" || $(this).css("background-color") == "rgb(250, 250, 250)")
+        
+        if ($(this).css("background-color") == "rgb(255, 255, 255)" || $(this).css("background-color") == "rgb(250, 250, 250)" || $(this).css("background-color") == "rgb(244, 244, 244)" || $(this).css("background-color") == "rgb(243, 255, 242)" || $(this).css("background-color") == "rgb(239, 246, 252)" || $(this).css("background-color") == "rgb(253, 250, 233)")
             $(this).css("background-color", "#111111");
     });
 }
